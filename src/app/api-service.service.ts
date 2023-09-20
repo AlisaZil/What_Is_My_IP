253291c7a6a2cc53424ac344b77f0caf372b34d4
Alisa Zilberman <alisazilberman@Alisas-MacBook-Air.local>
2023-09-21 02:26:28 +0300

    add(get user ip method)

---
+++ src/app/api-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServieService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  readonly APIurl = 'https://geo.ipify.org/api/v2/country?apiKey=at_asKIjxx9xN2PGwv0JE5vdOc8hhQK8';
+
+  getUserIp(): Observable<any>{
+    return this.httpClient.get<any>(this.APIurl);
+  }
+}
