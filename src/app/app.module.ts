59a3c0903e866bee065186d0dc71395454a68024
AlisaZil <alisazilberman@gmail.com>
2023-09-21 07:51:53 -0700

    Add(card detail card)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { DetailCardComponent } from './detail-card/detail-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    DetailCardComponent
   ],
   imports: [
     BrowserModule,
