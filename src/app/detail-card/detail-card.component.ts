2914ddd6afee96b6e70114587dcf3cc84d04d91d
AlisaZil <alisazilberman@gmail.com>
2023-09-21 09:06:15 -0700

    add(secondary section )

--- src/app/detail-card/detail-card.component.ts
+++ src/app/detail-card/detail-card.component.ts
@@ -11,13 +11,32 @@ export class DetailCardComponent {
 
   public ip: string|undefined = undefined;
   public location: string|undefined = undefined;
-  public timeZone: string|undefined = undefined;
+  public timeZone: string | undefined = undefined;
+  
+  public operatingSystem: string | undefined = undefined;
+  public ISP: string | undefined = undefined;
+  public connectionType: string | undefined = undefined;
 
   ngOnChanges(): void {
     this.ip = this.ipObj.ip;
     this.location = `${this.ipObj.location.city}, ${this.ipObj.location.country}`;
     this.timeZone = this.ipObj.location.timezone;
+
+    this.operatingSystem = this.getUserOperatingSystem(navigator.appVersion);
+    this.ISP = this.ipObj.as.name;
+    this.connectionType = this.ipObj.as.type
+    console.log(this.ipObj);
     
+  }
+
+  getUserOperatingSystem(operatingSystem: string) {
+    if (operatingSystem.includes('Windows')) {
+      return 'Windows';
+    }
+    if (operatingSystem.includes('Mac')) {
+      return 'Mac';
+    }
+    return undefined;
   }
 
 }
